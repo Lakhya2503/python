111715ad138073676eb82d53682ff61591e1dc3e
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-06 00:23:06 +0530

    add: enhance comments in BalanceSheet class to clarify private variable usage

--- OOP/basic_03.py
+++ OOP/basic_03.py
@@ -4,7 +4,13 @@
 
 class BalanceSheet:
     def __init__(self):
-        self.__balance = 0
+        self.__balance = 0 
+        # the __balance is private for other but not for our class 
+        # when you wan't to access the __balance 
+        # you can't directly write the .__balace beacuse 
+        # when you put the __ in any variable name on first
+        # then it's a private variable for other but not for our 
+        # class so you will create the getter setter method for access on outsice the class
 
     def get_balance(self):
         return f"The balance is: {self.__balance}.00 Rs/-"
