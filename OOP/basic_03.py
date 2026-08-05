844abe79b4a6a8287cea818fa03c7c2636c93740
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-06 00:18:38 +0530

    add: implement BalanceSheet class with getter and setter for balance

--- OOP/basic_03.py
+++ OOP/basic_03.py
@@ -1,1 +1,18 @@
 # Encapsulation
+
+# create getter and setter
+
+class BalanceSheet:
+    def __init__(self):
+        self.__balance = 0
+
+    def get_balance(self):
+        return f"The balance is: {self.__balance}.00 Rs/-"
+
+    def set_balance(self,balance):    
+        self.__balance = balance
+
+
+bank_of_baroda_account = BalanceSheet()
+bank_of_baroda_account.set_balance(5000000)
+print(bank_of_baroda_account.get_balance())
