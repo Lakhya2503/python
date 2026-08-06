48c4defdda70462c68888a9a298ee17b10fdf240
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-06 15:18:36 +0530

    add: initialize total_car in Car class and update fule_type method; create basic_05.py with polymorphism comment

--- OOP/basic_04.py
+++ OOP/basic_04.py
@@ -1,9 +1,15 @@
 # polymorphism
 
 class Car:
+
+    total_car = 0
+
     def __init__(self, brand, model):
         self.model = model
         self.brand = brand
+        Car.total_car += 1 
+        # can't add the self beacuse when i Car.total_car then he will access the 
+        # main total_car when Car.total_car then count the total_car created
 
     def fule_type(self):
         return f" Petrol or Diesel "
@@ -21,3 +27,5 @@ print(f"my_x1 : {my_x1.fule_type()}")
 
 my_atto = ElectricalCar("BYD","Atto 3")
 print(f"my_atto : {my_atto.fule_type()}")
+
+print(Car.total_car) 
