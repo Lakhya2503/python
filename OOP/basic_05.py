16923f32b1bebe4981508802ea2f955610bae92b
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-06 15:32:56 +0530

    add: implement Car class with static method for car description in basic_05.py

--- OOP/basic_05.py
+++ OOP/basic_05.py
@@ -1,1 +1,16 @@
-# polymorphism
+# static methods
+
+
+class Car:
+    def __init__(self, brand, model):
+        self.model = model
+        self.brand = brand
+
+    @staticmethod 
+    # static method are decorator of python this will use for static method
+    def car_description():
+        return "car was good for traveling... 🚙"
+
+
+
+print(Car.car_description())
