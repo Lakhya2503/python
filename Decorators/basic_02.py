54a50a67a107cc09afca28e7e05f7a037f095a38
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-07 16:13:40 +0530

    add: implement debugging decorator in basic_02.py

--- Decorators/basic_02.py
+++ Decorators/basic_02.py
@@ -1,2 +1,16 @@
 # Debugging Function Calls
 
+def dubbug(func):
+    def wrapper(*args, **kwargs):
+        args_value = ', '.join(str(arg) for args in args)
+        return func(*args, **kwargs)
+    return wrapper
+
+def hello():
+    print("hello")
+
+def greet(name, greeting="Hello"):
+    print(f"{greeting}, {name}")
+
+
+greet("Laxman", greeting="what's your poisen..??")
